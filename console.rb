aaa44b8e2f4879a69a2ed630dbad08af7e9e5f8e
Castlecelts <jamesdrmundie@gmail.com>
2018-11-02 14:04:25 +0000

    CRUD complete for Films

--- console.rb
+++ console.rb
@@ -1,10 +1,11 @@
 require_relative('./models/customer')
 # require_relative('models/ticket')
-# require_relative('models/film')
+require_relative('models/film')
 
 require('pry-byebug')
 
 Customer.delete_all
+Film.delete_all
 
 
 customer1 = Customer.new({
@@ -29,4 +30,28 @@ customer3.save
 
 # customer2.delete
 
-p Customer.all
+# p Customer.all
+
+film1 = Film.new({
+  'title' => 'Matrix',
+  'price' => 4
+})
+film2 = Film.new({
+  'title' => 'The Hobbit',
+  'price' => 10
+})
+film3 = Film.new({
+  'title' => 'Die Hard',
+  'price' => 8
+})
+
+film1.save
+film2.save
+film3.save
+
+# film1.price = 40
+# film1.update
+
+# film2.delete
+#
+# p Film.all
